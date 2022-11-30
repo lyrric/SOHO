ab11f7527b10e68f509cd2a8c4b81b5791945fb6
xiaodong.wang <xiaodong@wemew.cn>
2022-11-30 17:29:09 +0800

    test

--- src/main/java/io/github/lyrric/App.java
+++ src/main/java/io/github/lyrric/App.java
@@ -19,25 +19,26 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class App {
 
     private static String ukey ="85be376d0670f10bb8a77916bf774e53";
-    private static String tokenId="3bb5594fb56bf13512c7db5579ad803a";
-    private static String productID="30660e54-e048-409e-bb21-2d9d96381b84";
+    private static String tokenId="e36ffe240b3636071f123f43c9139844";
+    private static String productID="9af5689f-c93f-4d8f-9c46-ac01fcb43e43";
 
     private static final AtomicBoolean status = new AtomicBoolean(false);
 
-    static final ExecutorService pool = Executors.newFixedThreadPool(5);
+    static final ExecutorService pool = Executors.newFixedThreadPool(10);
 
     private static final String URL = "https://sapph5api.leqilucky.com/Sale/Task/SoHo";
 
     public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
         /*System.out.println("请输入ukey：");
-        ukey =  sc.nextLine().trim();*/
-        System.out.println("请输入tokenId：");
+        ukey =  sc.nextLine().trim();
+        System.out.println("请输入tokenId：");*/
         tokenId =  sc.nextLine().trim();
         System.out.println("请输入productID：");
         productID =  sc.nextLine().trim();
@@ -46,13 +47,17 @@ public class App {
 
         System.out.println("开始抢购中......");
         MyTask task = new MyTask();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             pool.submit(task);
         }
         pool.shutdown();
+        pool.awaitTermination(1, TimeUnit.MINUTES);
         if (!status.get()) {
             System.out.println("抢购失败");
+        }else{
+            System.out.println("抢购成功");
         }
+
     }
 
 
@@ -71,22 +76,25 @@ public class App {
     }
 
     public static void post() throws IOException {
+
         String params = String.format("uname=h5&ukey=%s&tokenid=%s&productID=%s&isFreeCard=true&isFirst=true&IsCfmdLimitArea=false&isFreeCardLimitRemind=false",
                 ukey, tokenId, productID);
         HttpPost post = new HttpPost(URL + "?" + params);
         List<Header> headers = Config.HEADERS;
 
         post.setHeaders(headers.toArray(new Header[0]));
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        CloseableHttpResponse response = httpClient.execute(post);
-        HttpEntity httpEntity = response.getEntity();
-        String json =  EntityUtils.toString(httpEntity, StandardCharsets.UTF_8);
-        JSONObject jsonObject = JSONObject.parseObject(json);
-        if (jsonObject.containsKey("ErrMsg") && jsonObject.getString("ErrMsg") != null) {
-            System.out.println(LocalDateTime.now() + " 抢购失败:" + jsonObject.getString("ErrMsg"));
-        }else{
-            System.out.println(LocalDateTime.now() + " 抢购成功！");
-            status.set(true);
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()){
+            System.out.println(Thread.currentThread().getId() + "-----" + LocalDateTime.now() + " 开始抢购");
+            CloseableHttpResponse response = httpClient.execute(post);
+            HttpEntity httpEntity = response.getEntity();
+            String json =  EntityUtils.toString(httpEntity, StandardCharsets.UTF_8);
+            JSONObject jsonObject = JSONObject.parseObject(json);
+            if (jsonObject.containsKey("ErrMsg") && jsonObject.getString("ErrMsg") != null) {
+                System.out.println(Thread.currentThread().getId() + "-----" + LocalDateTime.now() + " 抢购失败:" + jsonObject.getString("ErrMsg"));
+            }else{
+                System.out.println(Thread.currentThread().getId() + "-----" + LocalDateTime.now() + " 抢购成功！");
+                status.set(true);
+            }
         }
     }
 }
