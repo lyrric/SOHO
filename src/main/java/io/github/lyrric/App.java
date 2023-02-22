0508484cafc98701d7b5c3d460a93d5cb0e414c9
xiaodong.wang <xiaodong@wemew.cn>
2023-02-22 16:14:36 +0800

    定时抢购

--- src/main/java/io/github/lyrric/App.java
+++ src/main/java/io/github/lyrric/App.java
@@ -1,5 +1,6 @@
 package io.github.lyrric;
 
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
@@ -11,12 +12,8 @@ import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -25,8 +22,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class App {
 
     private static String ukey ="85be376d0670f10bb8a77916bf774e53";
-    private static String tokenId="e36ffe240b3636071f123f43c9139844";
-    private static String productID="9af5689f-c93f-4d8f-9c46-ac01fcb43e43";
+    private static String tokenId="fd70bf4f1aa6025d2990574cd975cc18";
+    private static String productID="bad0b5f1-36e9-441a-b33c-017af055e87d";
 
     private static final AtomicBoolean status = new AtomicBoolean(false);
 
@@ -37,26 +34,38 @@ public class App {
     public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
         /*System.out.println("请输入ukey：");
-        ukey =  sc.nextLine().trim();
-        System.out.println("请输入tokenId：");*/
+        ukey =  sc.nextLine().trim();*/
+        System.out.println("请输入tokenId：");
         tokenId =  sc.nextLine().trim();
         System.out.println("请输入productID：");
         productID =  sc.nextLine().trim();
         System.out.println("按回车键开始抢购");
         sc.nextLine();
 
-        System.out.println("开始抢购中......");
-        MyTask task = new MyTask();
-        for (int i = 0; i < 10; i++) {
-            pool.submit(task);
-        }
-        pool.shutdown();
-        pool.awaitTermination(1, TimeUnit.MINUTES);
-        if (!status.get()) {
-            System.out.println("抢购失败");
-        }else{
-            System.out.println("抢购成功");
-        }
+        Date startDate = DateUtil.parseDateTime("   2023-02-22 15:59:57").toJdkDate();
+        Timer timer = new Timer(false);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(LocalDateTime.now()+"    开始抢购中......");
+                MyTask task = new MyTask();
+                for (int i = 0; i < 10; i++) {
+                    pool.submit(task);
+                }
+                pool.shutdown();
+                try {
+                    pool.awaitTermination(1, TimeUnit.MINUTES);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                if (!status.get()) {
+                    System.out.println(LocalDateTime.now()+"    抢购失败");
+                }else{
+                    System.out.println(LocalDateTime.now()+"    抢购成功");
+                }
+            }
+        }, startDate);
+
 
     }
 
@@ -65,7 +74,7 @@ public class App {
         @Override
         public void run() {
             long start = System.currentTimeMillis();
-            while (!status.get() && System.currentTimeMillis() - start < 5000) {
+            while (!status.get() && System.currentTimeMillis() - start < 6000) {
                 try {
                     post();
                 } catch (IOException e) {
